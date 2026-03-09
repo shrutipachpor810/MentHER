f83d0b9f7f9e44e974a444597913105fe72824bf
prachi-madane14 <prachimadane2004@gmail.com>
2026-03-09 14:46:08 +0530

    mongodb error solved

--- server/index.js
+++ server/index.js
@@ -31,9 +31,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.error("MongoDB connection error:", err));
 
 // REST API routes
 app.use('/api/auth', authRoutes);
